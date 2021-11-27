70b93437edabff25e948433f6729d97f6b9255a5
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-27 13:32:26 +0200

    add connection to db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Scopics/architecture-lab-3
 
 go 1.17
+
+require github.com/lib/pq v1.10.4
--- server/db/db.go
+++ server/db/db.go
@@ -1,1 +1,29 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Connection struct {
+	Host       string
+	Port       string
+	User       string
+	Password   string
+	DbName     string
+	DisableSSL bool
+}
+
+func (c *Connection) GetConnectionString() string {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		c.Host, c.Port, c.User, c.Password, c.DbName)
+
+	return connectionString
+}
+
+func (c *Connection) Open() (*sql.DB, error) {
+	return sql.Open("postgres", c.GetConnectionString())
+}
