8e71fcdd143a70758cd1ff344495680181099a3d
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-27 15:10:54 +0200

    add func AddNewOrder to data.go

--- server/restaurant/data.go
+++ server/restaurant/data.go
@@ -11,12 +11,17 @@ type MenuItem struct {
 }
 
 type Order struct {
-	Id                   int64   `json:"id"`
-	Table                int64   `json:"table"`
+	Id                   int     `json:"id"`
+	Table                int     `json:"table"`
 	Date                 string  `json:"date"`
 	TotalPrice           float64 `json:"totalPrice"`
 	TotalPriceWithoutTax float64 `json:"totalPriceWithoutTax"`
 	RecommendedTip       float64 `json:"recommendedTip"`
+}
+
+type OrderItem struct {
+	ItemId   int64 `json:"itemId"`
+	Quantity int64 `json:"quantity"`
 }
 
 type Store struct {
@@ -48,5 +53,31 @@ func (s *Store) GetMenu() ([]*MenuItem, error) {
 		menu = make([]*MenuItem, 0)
 	}
 	return menu, nil
+}
 
+func (s *Store) AddNewOrder(table int, items []*OrderItem) (*Order, error) {
+	orderRow := s.Db.QueryRow("INSERT INTO orders (\"table\") VALUES ($1) RETURNING id", table)
+
+	var orderId int
+	err := orderRow.Scan(&orderId)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range items {
+		s.Db.QueryRow("INSERT INTO order_details (order_id, meal_id, quantity) VALUES ($1, $2, $3)", orderId, item.ItemId, item.Quantity)
+	}
+
+	var totalPrice, totalPriceWithoutTax, recommendedTip float64
+	s.Db.QueryRow("SELECT * FROM get_total_price($1)", orderId).Scan(&orderId, &totalPrice, &totalPriceWithoutTax, &recommendedTip)
+
+	order := &Order{
+		Id:                   orderId,
+		Table:                table,
+		TotalPrice:           totalPrice,
+		TotalPriceWithoutTax: totalPriceWithoutTax,
+		RecommendedTip:       recommendedTip,
+	}
+
+	return order, nil
 }
