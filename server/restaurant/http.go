b6c1c71c1360729d17b87ea93495ec0bf523b9d0
MaksGovor <maksgovruha@gmail.com>
2021-11-27 15:27:41 +0200

    Add http hadler for full list menu

--- server/restaurant/http.go
+++ server/restaurant/http.go
@@ -1,1 +1,32 @@
 package restaurant
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Scopics/architecture-lab-3/server/tools"
+)
+
+// Channels HTTP handler.
+type HttpHandlerFunc http.HandlerFunc
+
+// HttpHandler creates a new instance of channels HTTP handler.
+func HttpHandler(store *Store) HttpHandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			handleFullMenu(store, rw)
+		} else {
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+func handleFullMenu(store *Store, rw http.ResponseWriter) {
+	res, err := store.GetMenu()
+	if err != nil {
+		log.Printf("Error in a database query: %s", err)
+		tools.WriteJsonInternalError(rw)
+		return
+	}
+	tools.WriteJsonOk(rw, res)
+}
