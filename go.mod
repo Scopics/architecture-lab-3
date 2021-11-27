3f606cae93fb52e04ce31858c4cf7458c26b1dfd
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-27 13:40:29 +0200

    add .env and params for connection to db

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Scopics/architecture-lab-3
 
 go 1.17
 
-require github.com/lib/pq v1.10.4
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.4
+)
--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"strings"
+
+	"github.com/Scopics/architecture-lab-3/server/db"
+	"github.com/joho/godotenv"
 )
 
 const defaultPort = 8000
+
+func NewDbConnection() (*sql.DB, error) {
+	host, _ := os.LookupEnv("HOST")
+	port, _ := os.LookupEnv("PORT")
+	user, _ := os.LookupEnv("USER")
+	password, _ := os.LookupEnv("PASSWORD")
+
+	connection := &db.Connection{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		DbName:   "restaurant",
+	}
+	return connection.Open()
+}
 
 func inputLoop(quit chan string) {
 	reader := bufio.NewReader(os.Stdin)
@@ -29,6 +49,12 @@ func interrruptLoop(quit chan string) {
 	signal.Notify(sigChannel, os.Interrupt)
 	<-sigChannel
 	quit <- "Interrupted server"
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Print("No .env file found")
+	}
 }
 
 func main() {
