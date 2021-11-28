a2b37c7c21d8cad051d39301293ed751938fc3fa
YAGoOaR <yegorgribenko68@gmail.com>
2021-11-28 14:42:35 +0200

    Add API server structure

---
+++ server/cmd/server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Scopics/architecture-lab-3/server/restaurant"
+)
+
+type APIServer struct {
+	port    int
+	handler restaurant.HttpHandlerFunc
+	server  *http.Server
+}
+
+func (s *APIServer) Start() error {
+	handler := new(http.ServeMux)
+
+	s.server = &http.Server{
+		Addr:    fmt.Sprintf(":%d", s.port),
+		Handler: handler,
+	}
+
+	handler.HandleFunc("/restaurant", s.handler)
+	return s.server.ListenAndServe()
+}
+
+func (s *APIServer) Stop() error {
+	if s.server == nil {
+		return fmt.Errorf("server was not started")
+	}
+	return s.server.Shutdown(context.Background())
+}
